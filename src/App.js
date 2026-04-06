765a790354d49c02cbd685be215055ad706a6f29
Ayush Choudhary <ka843004@gmail.com>
2026-04-06 23:46:53 +0530

    workign section added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Landing from './components/Landing';
 import BodyPage from './components/BodyPage';
 import Pricing from './components/Pricing';
+import Workingsection from './components/Workingsection';
 
 function App() {
   return (
@@ -19,6 +20,11 @@ function App() {
       <section className="pricing-section">
         <Pricing />
       </section>
+
+      <section className="working-section">
+        <Workingsection />
+      </section>
+
     </>
 
   );
---
+++ src/components/Workingsection.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+export default function WorkWithUsSection() {
+  return (
+    <div className="relative py-24 overflow-hidden bg-gray-900 isolate sm:py-32">
+      <img
+        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+        alt=""
+        className="absolute inset-0 object-cover object-right -z-10 size-full md:object-center"
+      />
+
+      <div
+        aria-hidden="true"
+        className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+      >
+        <div
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+          className="aspect-[1097/845] w-[274.25px] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+        />
+      </div>
+
+      <div
+        aria-hidden="true"
+        className="absolute -translate-x-1/2 -top-52 left-1/2 -z-10 transform-gpu blur-3xl sm:-top-112 sm:ml-16 sm:translate-x-0"
+      >
+        <div
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+          className="aspect-[1097/845] w-[274.25px] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+        />
+      </div>
+
+      <div className="px-6 mx-auto max-w-7xl lg:px-8">
+        <div className="max-w-2xl mx-auto lg:mx-0">
+          <h2 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">
+            Work with us
+          </h2>
+          <p className="mt-8 text-lg font-medium text-gray-300 sm:text-xl/8">
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat.
+          </p>
+        </div>
+
+        <div className="max-w-2xl mx-auto mt-10 lg:mx-0 lg:max-w-none">
+          <div className="grid grid-cols-1 text-base font-semibold text-white gap-x-8 gap-y-6 sm:grid-cols-2 md:flex lg:gap-x-10">
+            <a href="#">Open roles →</a>
+            <a href="#">Internship program →</a>
+            <a href="#">Our values →</a>
+            <a href="#">Meet our leadership →</a>
+          </div>
+
+          <dl className="grid grid-cols-1 gap-8 mt-16 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+            <div className="flex flex-col-reverse gap-1">
+              <dt className="text-base text-gray-300">Offices worldwide</dt>
+              <dd className="text-4xl font-semibold tracking-tight text-white">12</dd>
+            </div>
+
+            <div className="flex flex-col-reverse gap-1">
+              <dt className="text-base text-gray-300">Full-time colleagues</dt>
+              <dd className="text-4xl font-semibold tracking-tight text-white">300+</dd>
+            </div>
+
+            <div className="flex flex-col-reverse gap-1">
+              <dt className="text-base text-gray-300">Hours per week</dt>
+              <dd className="text-4xl font-semibold tracking-tight text-white">40</dd>
+            </div>
+
+            <div className="flex flex-col-reverse gap-1">
+              <dt className="text-base text-gray-300">Paid time off</dt>
+              <dd className="text-4xl font-semibold tracking-tight text-white">Unlimited</dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </div>
+  );
+}
